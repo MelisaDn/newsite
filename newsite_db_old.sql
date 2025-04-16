113abfbfc6680cd040682016f9afb1671e9be0aa
MelisaDn <127612187+MelisaDn@users.noreply.github.com>
2025-04-16 22:20:30 +0300

    updating database

---
+++ newsite_db_old.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 15, 2023 at 10:01 AM
+-- Server version: 8.0.32
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `newsite_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsite`
+--
+
+CREATE TABLE `newsite` (
+  `site_id` int NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `dateofbirth` date NOT NULL,
+  `emailaddress` varchar(100) NOT NULL,
+  `contactnumber` varchar(15) NOT NULL,
+  `specialty_id` int NOT NULL,
+  `avatar_path` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `newsite`
+--
+
+INSERT INTO `newsite` (`site_id`, `firstname`, `lastname`, `dateofbirth`, `emailaddress`, `contactnumber`, `specialty_id`, `avatar_path`) VALUES
+(1, 'Amir', 'Kermani', '2000-05-30', 'amirkermani99@gmail.com', '09221942126', 4, NULL),
+(13, 'Melisa', 'Dn', '2001-01-01', 'melidn1010@gmail.com', '12345678', 3, 'uploads/12345678.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialties`
+--
+
+CREATE TABLE `specialties` (
+  `specialty_id` int NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `specialties`
+--
+
+INSERT INTO `specialties` (`specialty_id`, `name`) VALUES
+(1, 'Databaste Admin'),
+(2, 'Software Developer'),
+(3, 'Web Administrator'),
+(4, 'Other');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(4, 'admin', '80a19f669b02edfbc208a5386ab5036b');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `newsite`
+--
+ALTER TABLE `newsite`
+  ADD PRIMARY KEY (`site_id`),
+  ADD KEY `fk_newsite_specialties` (`specialty_id`);
+
+--
+-- Indexes for table `specialties`
+--
+ALTER TABLE `specialties`
+  ADD PRIMARY KEY (`specialty_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `newsite`
+--
+ALTER TABLE `newsite`
+  MODIFY `site_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `specialties`
+--
+ALTER TABLE `specialties`
+  MODIFY `specialty_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `newsite`
+--
+ALTER TABLE `newsite`
+  ADD CONSTRAINT `fk_newsite_specialties` FOREIGN KEY (`specialty_id`) REFERENCES `specialties` (`specialty_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
